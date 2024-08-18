2b741a76ee698d695ca4511e47d9edfddadcdbd9
Grigoriy <foxtail25@gmail.com>
2024-08-19 00:38:49 +0300

    нащупал решение перезгрузки проигрователя при потери связи

--- src/component/Player.jsx
+++ src/component/Player.jsx
@@ -1,4 +1,4 @@
-import {  useLayoutEffect, useRef } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const title = document.querySelector('title')
@@ -9,14 +9,22 @@ export const Player = () => {
 
     let audio = useRef()
 
+    let [wait, setWait] = useState(0)
+
     useLayoutEffect(() => {
         if (audio.current) {
+            audio.current.pause()
             audio.current.play()
             title.innerText = `радио ${selectedStation.name}`
 
         }
     }, [selectedStation])
 
+    function reset() {
+        setWait(wait++)
+    }
+
+    console.log('reset')
 
     return <>
         <div>Player</div>
@@ -27,7 +35,23 @@ export const Player = () => {
                     <div>
                         Играет радио {selectedStation.name}
                     </div>
-                    <audio ref={audio} controls src={selectedStation.radioDot.dot_1.href}></audio>
+                    <audio
+                        ref={audio}
+                        controls
+                        src={selectedStation.radioDot.dot_1.href}
+
+                        // onPlaying={()=>console.log('onPlaying')}
+                        onCanPlay={()=>console.log('onCanPlay то что нужно!!! сигнализирует о возможности играть трек')}
+                        // onError={()=>console.log('Error')}
+                        // onSeeking={()=>console.log('onSeeking')}
+                        // onSeeked={()=>console.log('onSeeked')}
+                        //----------------------------------------------
+                        onProgress={()=>console.log('onProgress')}
+                        onSuspend={()=>console.log('onSuspend')}
+                        //-----------------------------------------------
+                        // onStalled={()=>{console.log('onStalled'); reset()}}
+                        onWaiting={()=>{console.log('onWaiting'); reset()}}
+                    ></audio>
                 </div>
                 :
                 <div>Радиостанция не выбрана</div>
