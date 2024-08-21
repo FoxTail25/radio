ef5f93d699e1257ba1977a0d5a17768255f72e40
Grigoriy <foxtail25@gmail.com>
2024-08-21 03:44:59 +0300

    уже не помню что

--- src/features/station/stationSlice.js
+++ src/features/station/stationSlice.js
@@ -2,22 +2,29 @@ import { createSlice } from "@reduxjs/toolkit";
 import radioStation from '../../data/station.js';
 import localDataWork from "../../utils/localStor.js";
 
-radioStation.forEach(e => {
-    let favorit = localDataWork.getFavoriteArr()
-    if (favorit.includes(e.name))
-        e.favorites = true
-})
+// radioStation.forEach(e => {
+//     let favorit = localDataWork.getFavoriteArr()
+//     if (favorit.includes(e.name))
+//         e.favorites = true
+// })
 
 export const stationSlice = createSlice({
     name: 'selectedStation',
     initialState: {
         user_selected_station: '',
-        all_radioStation: radioStation,
+        all_radioStation: radioStation.map(e => {
+            let favorit = localDataWork.getFavoriteArr()
+            if (favorit.includes(e.name)) {
+
+                e.favorites = true
+            }
+            return e
+        }),
     },
     reducers: {
         userSelected: (state, action) => { state.user_selected_station = action.payload },
 
-        favoriteFilter: (state, action) => {
+        favoriteFilter: (state) => {
             state.all_radioStation = state.all_radioStation.filter(e =>
                 e.favorites === true
             )
