825cc8ab63d8c0b102c2f9510edd14de15f22db4
Fox Tail <foxtail25@gmail.com>
2024-12-10 14:46:35 +0300

    add bisnesFM

--- src/data/station.js
+++ src/data/station.js
@@ -389,7 +389,20 @@ const radioStation = [
         }
 
     },
-    
+    {
+        name: "Радио БизнесFM",
+        category: ["pop"],
+        favorites: false,
+        img: 'assets/image/radio/180/bfm-russia.png',
+        radioDot: {
+            dot_1: {
+                href: "https://bfm.hostingradio.ru:9075/fm",
+                format: '',
+                location: "",
+            },
+        }
+
+    },
 ]
 
 export default radioStation
